af93e3ef1b9a2a35608171b5418ce0cf05dd065c
Nicholas Bishop <nicholasbishop@gmail.com>
2018-04-12 17:09:40 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setuptools.setup(
     name='ec2window',
     author='Nicholas Bishop',
     author_email='nicholasbishop@gmail.com',
-    version='1.0.0',
+    version='1.0.1',
     description='Simple EC2 instance context manager',
     url='https://github.com/nicholasbishop/ec2window')
