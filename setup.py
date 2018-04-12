a1df326f1af39ffd8c3b2c30756c72eb8911c16c
Nicholas Bishop <nicholasbishop@gmail.com>
2018-04-12 17:08:58 -0400

    Update metadata

--- setup.py
+++ setup.py
@@ -6,5 +6,8 @@ import setuptools
 
 setuptools.setup(
     name='ec2window',
+    author='Nicholas Bishop',
+    author_email='nicholasbishop@gmail.com',
     version='1.0.0',
-    description='Simple EC2 instance context manager')
+    description='Simple EC2 instance context manager',
+    url='https://github.com/nicholasbishop/ec2window')
